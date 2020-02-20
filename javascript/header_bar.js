9cfc8918905d4b234024b2361fb559617deed09a
Mariareynolds <mfrmostyn@hotmail.co.uk>
2020-02-20 13:02:27 +0800

    jQuery Header and cropped whitsundays image

--- javascript/header_bar.js
+++ javascript/header_bar.js
@@ -1,18 +1,26 @@
-var opacity = 1;
-var lastScrollTop = 0;
-$(window).scroll(function(){
-    var st = $(this).scrollTop();
-    if(st == 0)
-        $('#header_bar').css('opacity','1');
-
-    if (opacity > 0.5 && (st > lastScrollTop)){
-       $('#header_bar').css('opacity','-=0.01');
-       opacity -= 0.01;
+// var opacity = 0;
+// var lastScrollTop = 0;
+// $(window).scroll(function(){
+//     var st = $(this).scrollTop();
+//     if(st == 0)
+//         $('#header_bar').css('opacity','0');
+//
+//     if (opacity < 0.5 && (st > lastScrollTop)){
+//        $('#header_bar').css('opacity','+=0.01');
+//        opacity += 0.01;
+//     }
+//     else if(opacity > 1)
+//     {
+//        $('#header_bar').css('opacity','-=0.01');
+//         opacity -= 0.01;
+//     }
+//     lastScrollTop = st;
+// });
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+        $("#header_bar").addClass("not-transparent");
     }
-    else if(opacity < 1)
-    {
-       $('#header_bar').css('opacity','+=0.01');
-        opacity += 0.01;
+    else {
+        $("#header_bar").removeClass("not-transparent");
     }
-    lastScrollTop = st;
 });
